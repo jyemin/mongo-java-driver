1ef287fa79d15aa4c957b1dfbb3f81f5dd357fd8
Jeff Yemin <jeff.yemin@10gen.com>
2018-01-11 12:15:34 -0500

    JAVA-2707: Add MongoClients factory and MongoClient interface to the driver module
       This will allow new applications to have no dependencies on the legacy API.

---
+++ driver/src/test/functional/com/mongodb/client/Fixture.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2018 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mongodb.client;
+
+import com.mongodb.ConnectionString;
+
+/**
+ * Helper class for the acceptance tests.  Considering replacing with MongoClientTestBase.
+ */
+public final class Fixture {
+    public static final String DEFAULT_URI = "mongodb://localhost:27017";
+    public static final String MONGODB_URI_SYSTEM_PROPERTY_NAME = "org.mongodb.test.uri";
+    private static final String DEFAULT_DATABASE_NAME = "JavaDriverTest";
+
+    private static MongoClient mongoClient;
+    private static ConnectionString connectionString;
+    private static MongoDatabase defaultDatabase;
+
+    private Fixture() {
+    }
+
+    public static synchronized MongoClient getMongoClient() {
+        if (mongoClient == null) {
+            ConnectionString connectionString = getConnectionString();
+            mongoClient = MongoClients.create(connectionString);
+            Runtime.getRuntime().addShutdownHook(new ShutdownHook());
+        }
+        return mongoClient;
+    }
+
+    @SuppressWarnings("deprecation") // This is for access to the old API, so it will use deprecated methods
+    public static synchronized MongoDatabase getDefaultDatabase() {
+        if (defaultDatabase == null) {
+            defaultDatabase = getMongoClient().getDatabase(getDefaultDatabaseName());
+        }
+        return defaultDatabase;
+    }
+
+    public static String getDefaultDatabaseName() {
+        return DEFAULT_DATABASE_NAME;
+    }
+
+    static class ShutdownHook extends Thread {
+        @Override
+        public void run() {
+            synchronized (Fixture.class) {
+                if (mongoClient != null) {
+                    if (defaultDatabase != null) {
+                        defaultDatabase.drop();
+                    }
+                    mongoClient.close();
+                    mongoClient = null;
+                }
+            }
+        }
+    }
+
+    public static synchronized String getConnectionStringProperty() {
+        String mongoURIProperty = System.getProperty(MONGODB_URI_SYSTEM_PROPERTY_NAME);
+        return mongoURIProperty == null || mongoURIProperty.isEmpty()
+               ? DEFAULT_URI : mongoURIProperty;
+    }
+
+    public static synchronized ConnectionString getConnectionString() {
+        if (connectionString == null) {
+            // TODO: add support for sslInvalidHostNameAllowed on Java 6
+            connectionString = new ConnectionString(getConnectionStringProperty());
+        }
+        return connectionString;
+    }
+}
