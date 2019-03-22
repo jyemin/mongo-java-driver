fc0d15d3dcfbdc5ce25154c3a950ee6d93ec5509
Jeff Yemin <jeff.yemin@10gen.com>
2019-03-22 10:04:03 -0400

    Move ChangeStreamLevel enum to an internal package
    JAVA-3183

--- driver-async/src/test/unit/com/mongodb/async/client/MongoCollectionSpecification.groovy
+++ driver-async/src/test/unit/com/mongodb/async/client/MongoCollectionSpecification.groovy
@@ -41,7 +41,6 @@ import com.mongodb.client.model.AggregationLevel
 import com.mongodb.client.model.BulkWriteOptions
 import com.mongodb.client.model.Collation
 import com.mongodb.client.model.CountOptions
-import com.mongodb.internal.client.model.CountStrategy
 import com.mongodb.client.model.CreateIndexOptions
 import com.mongodb.client.model.DeleteManyModel
 import com.mongodb.client.model.DeleteOneModel
@@ -62,10 +61,11 @@ import com.mongodb.client.model.ReplaceOptions
 import com.mongodb.client.model.UpdateManyModel
 import com.mongodb.client.model.UpdateOneModel
 import com.mongodb.client.model.UpdateOptions
-import com.mongodb.client.model.changestream.ChangeStreamLevel
 import com.mongodb.client.result.DeleteResult
 import com.mongodb.client.result.UpdateResult
 import com.mongodb.client.test.Worker
+import com.mongodb.internal.client.model.CountStrategy
+import com.mongodb.internal.client.model.changestream.ChangeStreamLevel
 import com.mongodb.operation.CountOperation
 import com.mongodb.operation.CreateIndexesOperation
 import com.mongodb.operation.DropCollectionOperation
