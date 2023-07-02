bac96c8179b5dc9d1796ebea33866380a4bda257
Jeff Yemin <jeff.yemin@mongodb.com>
2023-07-02 16:12:08 -0400

    Remove reflection from Java9DBCursorCleaner

--- driver-legacy/src/main/com/mongodb/Java9DBCursorCleaner.java
+++ driver-legacy/src/main/com/mongodb/Java9DBCursorCleaner.java
@@ -16,58 +16,26 @@
 
 package com.mongodb;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.lang.ref.Cleaner;
 
 import static com.mongodb.assertions.Assertions.assertNotNull;
 
-/**
- * A {@code java.lang.ref.Cleaner}-based implementation of {@link DBCursorCleaner}.  The implementation
- * is reflection-based so that it will compile with Java 8 even though  {@code java.lang.ref.Cleaner} was introduced in
- * Java 9.  {@link DBCursorCleaner#create(MongoClient, MongoNamespace, ServerCursor)} is responsible for ensuring that
- * this class is only used if the {@code java.lang.ref.Cleaner} class is available (i.e. the runtime is Java 9+).
- */
 final class Java9DBCursorCleaner extends DBCursorCleaner {
-    // Actual type is java.lang.ref.Cleaner
-    private static final Object CLEANER;
-    // Actual method is Cleaner#register(Object, Runnable)
-    private static final Method REGISTER_METHOD;
-    // Actual method is Cleanable#clean
-    private static final Method CLEAN_METHOD;
-
-    static {
-        try {
-            Class<?> cleanerClass = Class.forName("java.lang.ref.Cleaner");
-            CLEANER = cleanerClass.getMethod("create").invoke(null);
-            REGISTER_METHOD = cleanerClass.getMethod("register", Object.class, Runnable.class);
-            CLEAN_METHOD = Class.forName("java.lang.ref.Cleaner$Cleanable").getMethod("clean");
-        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
-            throw new MongoInternalException("Unexpected exception", e);
-        }
-    }
+    private static final Cleaner CLEANER = Cleaner.create();
 
     private final CleanerState cleanerState;
-    // Actual type is java.lang.ref.Cleaner$Cleanable
-    private final Object cleanable;
+    private final Cleaner.Cleanable cleanable;
 
     Java9DBCursorCleaner(final MongoClient mongoClient, final MongoNamespace namespace,
-                         final ServerCursor serverCursor) {
+            final ServerCursor serverCursor) {
         cleanerState = new CleanerState(mongoClient, namespace, serverCursor);
-        try {
-            cleanable = REGISTER_METHOD.invoke(CLEANER, this, cleanerState);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new MongoInternalException("Unexpected exception", e);
-        }
+        cleanable = CLEANER.register(this, cleanerState);
     }
 
     @Override
     void clearCursor() {
         cleanerState.clear();
-        try {
-            CLEAN_METHOD.invoke(cleanable);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new MongoInternalException("Unexpected exception", e);
-        }
+        cleanable.clean();
     }
 
     private static class CleanerState implements Runnable {
